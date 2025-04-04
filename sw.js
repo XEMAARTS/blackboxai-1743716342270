18aac7df87884bf343a6ccfcf8da70e736477017
XEMA ARTS MEDIA <xemaartsm@gmail.com>
2025-04-04 23:37:59 +0200

    Add sw.js 1743716379012

---
+++ sw.js
@@ -0,0 +1,23 @@
+// Basic service worker with offline fallback
+const CACHE_NAME = 'xema-arts-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/styles.css',
+  '/header.html',
+  '/footer.html'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
